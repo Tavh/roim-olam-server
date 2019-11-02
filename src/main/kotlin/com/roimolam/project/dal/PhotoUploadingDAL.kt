2a18afa4a0ca41cc4160909a4cafcee4b5676cca
Tav <tavherzlich26@gmail.com>
2019-11-02 18:22:47 +0200

    Access photo directory from env

--- src/main/kotlin/com/roimolam/project/dal/PhotoUploadingDAL.kt
+++ src/main/kotlin/com/roimolam/project/dal/PhotoUploadingDAL.kt
@@ -1,21 +1,21 @@
 package com.roimolam.project.dal
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Repository
 import org.springframework.web.multipart.MultipartFile
 import java.nio.file.Files
 import java.nio.file.Paths
 
 @Repository
-class PhotoUploadingDAL {
+class PhotoUploadingDAL (@Autowired val env: Environment) {
 
     fun saveCatalogItemPhoto(photo: MultipartFile): String {
-        val folder = "D:\\Repositories\\Git\\roim-olam-server\\src\\main\\resources\\catalog_item_images\\"
+        val folder = env.getProperty("CATALOG_PHOTO_DIRECTORY")
         val imageAsByteArray = photo.bytes
         val path = Paths.get(folder + photo.originalFilename)
         Files.write(path, imageAsByteArray)
 
         return photo.originalFilename.toString()
     }
-
-
 }
