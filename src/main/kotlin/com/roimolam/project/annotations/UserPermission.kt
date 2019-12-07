a2643861b070b3409c8dee1eb86eef6235ab9615
TavH <tavherzlich26@gmail.com>
2019-12-08 00:17:39 +0200

    Cleanup

--- src/main/kotlin/com/roimolam/project/annotations/UserPermission.kt
+++ src/main/kotlin/com/roimolam/project/annotations/UserPermission.kt
@@ -1,7 +1,6 @@
 package com.roimolam.project.annotations
 
 import com.roimolam.project.enums.UserType
-import java.lang.annotation.RetentionPolicy
 
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
