b90537c680b67ef6ba10d7e39763df9214852e85
Tavh <44731477+Tavh@users.noreply.github.com>
2020-05-02 17:53:02 +0300

    added test controller

--- src/main/kotlin/com/roimolam/project/controller/TestController.kt
+++ src/main/kotlin/com/roimolam/project/controller/TestController.kt
@@ -23,47 +23,11 @@ import org.springframework.web.multipart.MultipartFile
                     RequestMethod.PATCH ],
         exposedHeaders = [ "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "errorMessage" ])
 @RestController
-@RequestMapping("/catalog")
-class CatalogController (@Autowired val catalogItemFacade: CatalogItemLogicFacade,
-                         @Autowired val photoUploadingDAL: PhotoDAL) {
+@RequestMapping("/test")
+class TestController () {
 
-    @UserPermission(userType = UserType.ADMIN)
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/create-catalog-item")
-    fun createCatalogItem(@RequestBody catalogItemEntity: CatalogItemEntity): CatalogItemIDWrapper {
-        return catalogItemFacade.createCatalogItem(catalogItemEntity)
-    }
-
-    @UserPermission(userType = UserType.ADMIN)
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/save-catalog-item-photo")
-    fun saveCatalogItemPhoto(@ModelAttribute photo: MultipartFile): PhotoUploadStatusWrapper {
-        return photoUploadingDAL.saveCatalogItemPhoto(photo)
-    }
-
-    @GetMapping("/get-catalog-item/{id}")
-    fun getCatalogItem(@PathVariable id: Long): CatalogItemEntity? {
-        return catalogItemFacade.getCatalogItem(id)
-    }
-
-    @GetMapping("/get-all-catalog-items")
-    fun getAllCatalogItems(): List<CatalogItemEntity> {
-        return catalogItemFacade.getAllCatalogItems()
-    }
-
-    @GetMapping("/get-catalog-items-by-type")
-    fun getCatalogItemsByType(@RequestParam itemType: ItemType, @RequestParam(required = false) page: Int?): CatalogItemsWrapper {
-        return catalogItemFacade.getCatalogItemsByType(itemType, page)
-    }
-
-    @GetMapping("/get-catalog-items-by-brand-and-type")
-    fun getCatalogItemsByBrand(@RequestParam itemType: ItemType,
-                               @RequestParam brand: String): List<CatalogItemEntity> {
-        return catalogItemFacade.getCatalogItemsByBrand(itemType, brand)
-    }
-
-    @GetMapping("get-catalog-items-by-free-text")
-    fun getCatalogItemsByFreeText(@RequestParam freeText: String): List<CatalogItemEntity> {
-        return catalogItemFacade.getCatalogItemsByFreeText(freeText)
+    @GetMapping
+    fun getCatalogItemsByFreeText(): String {
+        return "Working!"
     }
 }
