b37d64dff685e65ce61829b43aa24d1d854b4dc4
Tav <tavherzlich26@gmail.com>
2020-06-16 16:05:19 +0300

    compression

--- src/main/kotlin/com/roimolam/project/constants/IOConstants.kt
+++ src/main/kotlin/com/roimolam/project/constants/IOConstants.kt
@@ -1,7 +1,11 @@
 package com.roimolam.project.constants
 
 const val DEFAULT_CATALOG_ITEM_PHOTO_NAME = "default.jpg"
+const val UNCOMPRESSED_PHOTO_NAME = "uncompressed_photo.jpg"
+const val COMPRESSED_PHOTO_NAME = "compressed_photo.jpg"
 const val DEFAULT_CATALOG_ITEM_PHOTO_PATH = "/src/main/resources/${DEFAULT_CATALOG_ITEM_PHOTO_NAME}"
+const val UNCOMPRESSED_PHOTO_PATH = "/src/main/resources/temp_photo_compression/${UNCOMPRESSED_PHOTO_NAME}"
+const val COMPRESSED_PHOTO_PATH = "/src/main/resources/temp_photo_compression/${COMPRESSED_PHOTO_NAME}"
 
 
 
--- src/main/kotlin/com/roimolam/project/dal/PhotoDAL.kt
+++ src/main/kotlin/com/roimolam/project/dal/PhotoDAL.kt
@@ -1,10 +1,10 @@
 package com.roimolam.project.dal
 
 
-import com.roimolam.project.constants.DEFAULT_CATALOG_ITEM_PHOTO_PATH
-import com.roimolam.project.constants.SPRING_MAIN_DIRECTORY_KEY
+import com.roimolam.project.constants.*
 import com.roimolam.project.data.PhotoUploadIdWrapper
 import com.roimolam.project.data.entities.CatalogItemPhotoEntity
+import org.apache.commons.io.FileUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Repository
@@ -13,8 +13,13 @@ import org.springframework.transaction.annotation.Transactional
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.zip.Deflater
+import java.util.zip.DeflaterOutputStream
+import javax.imageio.IIOImage
 import javax.imageio.ImageIO
+import javax.imageio.ImageWriteParam
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
@@ -25,21 +30,33 @@ class PhotoDAL (@Autowired val env: Environment,
 
     @Transactional(propagation = Propagation.REQUIRED)
     fun saveCatalogItemPhoto(catalogItemPhoto: CatalogItemPhotoEntity): PhotoUploadIdWrapper {
-        val photoAsByteArray = catalogItemPhoto.photoBase64
-        val baos = ByteArrayOutputStream()
-        val dfl = Deflater()
-        dfl.setLevel(Deflater.BEST_COMPRESSION)
-        dfl.setInput(photoAsByteArray)
-        val buffer = ByteArray(4 * 1024)
-        while (!dfl.finished()) {
-            val size = dfl.deflate(buffer)
-            baos.write(buffer, 0, size)
-        }
-        baos.close()
-        catalogItemPhoto.photoBase64 = baos.toByteArray()
+        compressBase64(catalogItemPhoto)
         entityManager.persist(catalogItemPhoto)
-
         return PhotoUploadIdWrapper(catalogItemPhoto.id)
+    }
+
+    private fun compressBase64(catalogItemPhoto: CatalogItemPhotoEntity) {
+        println("Initial image size : ${catalogItemPhoto.photoBase64.size}")
+        FileUtils.writeByteArrayToFile(File(UNCOMPRESSED_PHOTO_PATH), catalogItemPhoto.photoBase64)
+        val input = File(UNCOMPRESSED_PHOTO_PATH)
+        val image = ImageIO.read(input)
+        val output = File(COMPRESSED_PHOTO_PATH)
+        val out = FileOutputStream(output)
+        val writer = ImageIO.getImageWritersByFormatName("jpg").next()
+        val ios = ImageIO.createImageOutputStream(out)
+        writer.output = ios
+        val param = writer.defaultWriteParam
+        if (param.canWriteCompressed()) {
+            param.compressionMode = ImageWriteParam.MODE_EXPLICIT
+            param.compressionQuality = 0.05f
+        }
+
+        writer.write(null, IIOImage(image, null, null), param)
+        out.close()
+        ios.close()
+        writer.dispose()
+        catalogItemPhoto.photoBase64 = output.readBytes()
+        println("Compressed image size : ${catalogItemPhoto.photoBase64.size}")
     }
 
     fun getCatalogItemPhoto(id: String): ByteArray {
