c1aa48ce1d90ead1866a742199832990eedb04b7
Tav <tavherzlich26@gmail.com>
2020-06-17 13:29:17 +0300

    Removed useless constants

--- src/main/kotlin/com/roimolam/project/constants/EnvConstants.kt
+++ src/main/kotlin/com/roimolam/project/constants/EnvConstants.kt
@@ -1,6 +1,4 @@
 package com.roimolam.project.constants
 
-const val SPRING_ACTIVE_PROFILES = "spring.profiles.active"
 const val DEVELOPMENT_PROFILE_NAME = "dev";
-const val AWS_PROFILE_NAME = "prod-aws"
 const val SPRING_MAIN_DIRECTORY_KEY = "user.dir"
